fb246825b9808596363f126f9a4d8820c102c56d
Ethan Nitz <etnitz@gmail.com>
2022-03-28 15:35:50 -0500

    created routes

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const controllers = require('../controllers');
+const router = Router();
+
+router.get('/posts', controllers.getAllPosts);
+
+router.post('/createPosts', controllers.createPost);
+
+
+module.exports = router;
