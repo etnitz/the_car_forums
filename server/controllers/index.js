c10bbd92a44fd2084afd5d879d925ab129ea5532
Ethan Nitz <etnitz@gmail.com>
2022-03-29 13:00:34 -0500

    worked on controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -21,7 +21,51 @@ const getAllPosts = async (req, res) => {
     }
 }
 
+const getPostById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id)
+        if (post) {
+            return res.status(200).json({ post });
+        }
+        return res.status(404).send('Post with the specified ID does not exists');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const updatePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Post.findByIdAndUpdate(id, req.body, { new: true }, (err, post) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            if (!post) {
+                res.status(500).send('Plant not found!');
+            }
+            return res.status(200).json(post);
+        })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Post.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("Post deleted");
+        }
+        throw new Error("Post not found");
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
 module.exports = {
     createPost,
-    getAllPosts
+    getAllPosts,
+
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const controllers = require('../controllers');
+const controllers = require('../controllers/index');
 const router = Router();
 
 router.get('/posts', controllers.getAllPosts);
