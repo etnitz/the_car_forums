f3beabfd816efa1e52640a43065fdcee16e864b5
Ethan Nitz <etnitz@gmail.com>
2022-03-31 17:38:37 -0500

    made changes to PostCard

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,10 +1,12 @@
+
 const PostCard = (props) => {
 
     return (
         <div className="postCard">
             <h3>{props.title} </h3>
             <p>{props.content}</p>
-            <h4>{props.author}</h4>            
+            <h4>{props.author}</h4>
+            
         </div>
     )
 }
