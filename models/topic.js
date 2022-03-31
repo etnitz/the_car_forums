bc8e06261fdefd2fea468954b909b8ab611ef224
Ethan Nitz <etnitz@gmail.com>
2022-03-31 10:04:26 -0500

    working on components

--- models/topic.js
+++ models/topic.js
@@ -2,7 +2,6 @@ const { Schema } = require('mongoose')
 
 const Topic = new Schema(
     {
-        title: { type: String, required: true },
         category: { type: String, required: true },
         post: {type: Schema.Types.ObjectId, ref: 'posts' }
     },
