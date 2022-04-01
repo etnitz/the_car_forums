21d2765ea5f574a0cd74ddebcd51ae26c15287a7
Ethan Nitz <etnitz@gmail.com>
2022-03-31 19:29:03 -0500

    started connecting to heroku

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors())
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', routes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
